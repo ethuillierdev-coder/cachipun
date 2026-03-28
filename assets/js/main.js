52124a0f6aee9631a597fb2e1a5be0f73e7a9e4f
kike <ethuillierdev@gmail.com>
2026-03-27 23:05:34 -0300

    agregados los comentarios explicando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 alert('Juguemos un Cachipún')
 
 let seleccion1 = prompt('Jugador A: Elige 1 para Piedra, 2 para Papel o 3 para Tijera, por favor')
-if (seleccion1 !== '1' && seleccion1 !== '2' && seleccion1 !== '3') {
-    alert('Opción inválida, debes elegir 1, 2 o 3')
+if (seleccion1 !== '1' && seleccion1 !== '2' && seleccion1 !== '3') { // pido que si la elección no es igual a: entonces falla, en caso contrario, avanza al jugador B
+    alert('Opción inválida, debes elegir 1, 2 o 3') // si la opción no es válida, se termina el juego por que necesito que sea V solo 1 de las 3 opciones
 } else {
     let seleccion2 = prompt('Jugador B: Elige 1 para Piedra, 2 para Papel o 3 para Tijera, por favor')
 if (seleccion2 !== '1' && seleccion2 !== '2' && seleccion2 !== '3') {
     alert('Opción inválida Jugador B, debes elegir 1, 2 o 3')
 } else {
-    let resultado = ''
+    let resultado = '' // string vacío porque no se que valor tomará
 if (seleccion1 === seleccion2) {
     alert('Empate')
 }  else if (seleccion1 === '1' && seleccion2 === '2') {
